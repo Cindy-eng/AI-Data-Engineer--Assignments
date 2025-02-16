379aeaf7bf0f86621a2634c35b6136c05a48e035
Cindy-eng <cindylindi3@gmail.com>
2025-02-16 18:35:48 +0200

    just pratice

---
+++ Praticing/Numpy.py
@@ -0,0 +1,10 @@
+#First I need to know that Numpy we use tho handle mastrix (1D,2D)
+#Is packge python that provide methods and functions to handle array and matrix
+#Is is used in data analysis
+#Lets pratice
+
+#first we need to import 
+import Numpy as np
+# after this we can create array with one-dimetions ou two
+a=np.array([1,2,3]);
+print(a)
